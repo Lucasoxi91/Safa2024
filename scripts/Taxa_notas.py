d50f9d7a4ef76fdb4203d97595d21208fdd1053e
Lucasoxi91 <57160026+Lucasoxi91@users.noreply.github.com>
2024-05-07 11:06:10 -0300

    Update Taxa_notas.py

--- scripts/Taxa_notas.py
+++ scripts/Taxa_notas.py
@@ -22,79 +22,59 @@ def execute_query():
             with conn.cursor() as cur:
                 cur.execute("""
 WITH QuizData AS (
-	select 
-	i.name as curso,
-	ic2.name as escola,
-	ic2.id as escola_id,
-	q.name as simulado,
-	count( users.id) as n_user_id
-	,avg(qg.average) as grade_avg
-	from quiz_user_progresses qup  
-	inner join users on qup.user_id =  users.id 
-	inner join quizzes q on  qup.quiz_id = q.id
-	inner join institution_enrollments ie on  qup.user_id = ie.user_id
-	inner join institution_classrooms ic on ic.id =ie.classroom_id  
-	inner join institution_levels il on il.id = ic.level_id 
-	inner join institution_courses ic3 on ic3.id = il.course_id 
-	inner join institution_colleges ic2 on ic2.id =  ic3.institution_college_id  and ic2.id = ie.college_id 
-	inner join institutions i on i.id = ic2.institution_id  
-	inner join regions r on ic2.region_id =r.id 
-	inner join cities c on c.id =r.city_id 
-	inner join institutions_quizzes iq on iq.institution_id =i.id and iq.quiz_id = q.id
-	inner join quiz_grades qg on qg.user_id = users.id and qg.quiz_id = q.id 
-	where qup.finished = true and qup.quiz_id in (
-	  select iq.quiz_id  from institutions_quizzes iq where iq.institution_id in (
-		select distinct  i.id --, i.name,i.slug 
-		from institutions i 
-		inner join institution_colleges ic on ic.institution_id = i.id 
-		where i.holding_id =157 --safa
-		and ic.region_id =1063 -- tauá-CE
-		and ic.year =2023
-	  )
-	)
-	and ic2.year =2023
-	group by curso, escola, escola_id, simulado 
-	order by curso, escola, escola_id, simulado
-), EnrollmentData AS (
-	select  
-	i.name as curso,
-	count(ie.user_id) as matriculados,
-	ic2.name as escola,
-	ic2.id as escola_id
-	from institution_classrooms ic 
-	inner join institution_levels il on il.id = ic.level_id 
-	inner join institution_courses ic3 on ic3.id = il.course_id 
-	inner join institution_colleges ic2 on ic2.id =  ic3.institution_college_id  
-	inner join institutions i on i.id =  ic2.institution_id  
-	inner join regions r on ic2.region_id =r.id 
-	inner join cities c on c.id =r.city_id 
-	inner join institution_enrollments ie on ie.institution_id =i.id and ie.classroom_id =ic.id and ie.college_id =ic2.id 
-	--where  i.id in (244,280,321,246,284,322,324,245,287,323,325,326,247,283,285,286,327,330,331)
-	where  i.id in (
-		select distinct  i.id --, i.name,i.slug 
-		from institutions i 
-		inner join institution_colleges ic on ic.institution_id = i.id 
-		where i.holding_id =157 --safa
-		and ic.region_id =1063 -- tauá-CE
-		and ic.year =2023
-	)
-	and ic2.year =2023
-	--and ic2.id = 2509 -- CENTRO EDUCACIONAL BETESDA (for debug)
-	group by curso, escola, escola_id
-	order by curso, escola, escola_id
+    SELECT 
+        i.name as curso,
+        ic2.name as escola,
+        ic2.id as escola_id,
+        q.name as simulado,
+        count(users.id) as n_user_id,
+        avg(qg.average)::numeric(10,4) as grade_avg  -- Média de notas formatada com precisão
+    FROM 
+        quiz_user_progresses qup  
+    INNER JOIN users on qup.user_id = users.id 
+    INNER JOIN quizzes q on qup.quiz_id = q.id
+    INNER JOIN institution_enrollments ie on qup.user_id = ie.user_id
+    INNER JOIN institution_classrooms ic on ic.id = ie.classroom_id  
+    INNER JOIN institution_levels il on il.id = ic.level_id 
+    INNER JOIN institution_courses ic3 on ic3.id = il.course_id 
+    INNER JOIN institution_colleges ic2 on ic2.id = ic3.institution_college_id and ic2.id = ie.college_id 
+    INNER JOIN institutions i on i.id = ic2.institution_id  
+    INNER JOIN regions r on ic2.region_id = r.id 
+    INNER JOIN cities c on c.id = r.city_id 
+    INNER JOIN institutions_quizzes iq on iq.institution_id = i.id and iq.quiz_id = q.id
+    INNER JOIN quiz_grades qg on qg.user_id = users.id and qg.quiz_id = q.id 
+    WHERE qup.finished = true 
+    AND i.name ILIKE '%2024%'  -- Inclui todas as instituições com '2024' no nome
+    AND LOWER(ic2.name) NOT IN ('wiquadro', 'teste', 'escola demonstração', 'escola1', 'escola2')
+    GROUP BY i.name, ic2.name, ic2.id, q.name
+), 
+EnrollmentData AS (
+    SELECT  
+        i.name as curso,
+        count(ie.user_id) as matriculados,
+        ic2.name as escola,
+        ic2.id as escola_id
+    FROM 
+        institution_classrooms ic 
+    INNER JOIN institution_levels il on il.id = ic.level_id 
+    INNER JOIN institution_courses ic3 on ic3.id = il.course_id 
+    INNER JOIN institution_colleges ic2 on ic2.id = ic3.institution_college_id  
+    INNER JOIN institutions i on i.id = ic2.institution_id  
+    INNER JOIN institution_enrollments ie on ie.institution_id = i.id and ie.classroom_id = ic.id and ie.college_id = ic2.id 
+    WHERE i.name ILIKE '%2024%'  -- Inclui todas as instituições com '2024' no nome
+    AND LOWER(ic2.name) NOT IN ('wiquadro', 'teste', 'escola demonstração', 'escola1', 'escola2')
+    GROUP BY i.name, ic2.name, ic2.id
 )
 SELECT 
     qd.curso,
-    qd.escola_id,
     qd.escola,
     qd.simulado,
     qd.n_user_id as finalizaram,
     ed.matriculados,
-    CASE WHEN ed.matriculados > 0 then ( CAST(qd.n_user_id AS FLOAT) / ed.matriculados )* 100 ELSE NULL END AS participation,
-	CASE WHEN (CASE WHEN ed.matriculados > 0 then ( CAST(qd.n_user_id AS FLOAT) / ed.matriculados )* 100 ELSE NULL END) > 60 THEN TRUE ELSE FALSE END as filter_data
-	,qd.grade_avg
+    ROUND((CAST(qd.n_user_id AS numeric) / GREATEST(ed.matriculados, 1)) * 100, 4) AS participation,
+    qd.grade_avg  -- Adicionado à seleção para exibir na tabela final
 FROM QuizData qd
-inner JOIN EnrollmentData ed ON qd.curso = ed.curso AND qd.escola_id = ed.escola_id
+INNER JOIN EnrollmentData ed ON qd.curso = ed.curso AND qd.escola_id = ed.escola_id
 ORDER BY qd.curso, qd.escola, qd.simulado;
 
 
